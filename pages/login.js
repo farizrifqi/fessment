29b6896906cc410d8e5377052c7aaadeb61e00a2
zeraneL <farizrifqi26@gmail.com>
2023-05-01 06:24:10 +0800

    remove hard code auth

--- pages/login.js
+++ pages/login.js
@@ -8,8 +8,8 @@ export default function LoginPage() {
     let { data: session } = useSession()
 
 
-    const [email, setEmail] = useState("farizrifqi26@gmail.com")
-    const [password, setPassword] = useState("123123")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value)
@@ -31,55 +31,53 @@ export default function LoginPage() {
             toast(res.error, { hideProgressBar: true, autoClose: 2000, type: 'warning' })
         }
     }
-    if (!session) {
-        return (
-            <>
 
-                <div
-                    className="min-h-screen min-w-screen">
-                    <div className="flex flex-col ">
-                        <div className="md:flex md:flex-row h-screen md:items-center justify-around">
-                            <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md md:w-1/2 h-full">
-                                <div className="w-1/2 flex flex-col justify-center items-center gap-2">
-                                    <strong className="text-3xl uppercase md:mb-4 block tracking-wide">Log in</strong>
-                                    <input
-                                        id="email"
-                                        type="email"
-                                        placeholder="Enter Email"
-                                        className="border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded"
-                                        name="email"
-                                        value={email}
-                                        onChange={handleEmailChange}
+    return (
+        <>
 
-                                    ></input>
-                                    <input
-                                        id="password"
-                                        type="password"
-                                        placeholder="Password"
-                                        className={`border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded`}
-                                        value={password}
-                                        onChange={handlePasswordChange}
-                                    ></input>
-                                    <button className="text-white block bg-sky-500 p-3 w-full font-medium disabled:bg-sky-700" onClick={login}
+            <div
+                className="min-h-screen min-w-screen">
+                <div className="flex flex-col ">
+                    <div className="md:flex md:flex-row h-screen md:items-center justify-around">
+                        <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md md:w-1/2 h-full">
+                            <div className="w-1/2 flex flex-col justify-center items-center gap-2">
+                                <strong className="text-3xl uppercase md:mb-4 block tracking-wide">Log in</strong>
+                                <input
+                                    id="email"
+                                    type="email"
+                                    placeholder="Enter Email"
+                                    className="border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded"
+                                    name="email"
+                                    value={email}
+                                    onChange={handleEmailChange}
 
-                                    >Sign in</button>
-                                    <div className="flex flex-row justify-end w-full">
-                                        <Link href="register" className="tracking-wide" >Register new account</Link>
-                                    </div>
+                                ></input>
+                                <input
+                                    id="password"
+                                    type="password"
+                                    placeholder="Password"
+                                    className={`border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded`}
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                ></input>
+                                <button className="text-white block bg-sky-500 p-3 w-full font-medium disabled:bg-sky-700" onClick={login}
+
+                                >Sign in</button>
+                                <div className="flex flex-row justify-end w-full">
+                                    <Link href="register" className="tracking-wide" >Register new account</Link>
                                 </div>
-
                             </div>
-                            <div className="w-1/2 md:flex hidden flex-col items-center h-screen md:bg-[url('/assets/images/open-door.png')] bg-center bg-no-repeat">
-
-                            </div>
-
 
                         </div>
+                        <div className="w-1/2 md:flex hidden flex-col items-center h-screen md:bg-[url('/assets/images/open-door.png')] bg-center bg-no-repeat">
+
+                        </div>
+
+
                     </div>
                 </div>
-            </>
-        )
-    } else {
-        router.push('/')
-    }
+            </div>
+        </>
+    )
+
 }
--- pages/register.js
+++ pages/register.js
@@ -7,9 +7,9 @@ import Link from "next/link";
 export default function RegisterPage() {
     let router = useRouter()
     let { data: session } = useSession()
-    const [email, setEmail] = useState('farizrifqi26@gmail.com')
-    const [name, setName] = useState('Fariz Rifqi Ahmad')
-    const [repass, setPass] = useState({ pass: '123123', rePass: '123123' })
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [repass, setPass] = useState({ pass: '', rePass: '' })
 
     const validatePass = (p, r) => {
         setPass({ pass: p, rePass: r })
@@ -64,71 +64,69 @@ export default function RegisterPage() {
 
     useEffect(() => {
 
-    }, [validatePass, handleSetEmail])
-    if (session) {
-        return (
-            <>
-                <div
-                    className="min-h-screen min-w-screen">
-                    <div className="flex flex-col ">
-                        <div className="md:flex md:flex-row h-screen md:items-center justify-around">
-                            <div className="w-1/2 md:flex hidden flex-col items-center h-screen md:bg-[url('/assets/images/fill-form.png')] bg-center bg-no-repeat">
+    }, [])
 
-                            </div>
-                            <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md md:w-1/2 h-full">
-                                <div className="w-1/2 flex flex-col justify-center items-center gap-2">
-                                    <strong className="text-3xl uppercase md:mb-4 block tracking-wide">Register</strong>
-                                    <input
-                                        id="name"
-                                        type="text"
-                                        placeholder="Full Name"
-                                        className="border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded"
-                                        name="email"
-                                        value={name}
-                                        onChange={handleSetName}
-                                    ></input>
-                                    <input
-                                        id="email"
-                                        type="email"
-                                        placeholder="Enter Your Email"
-                                        className="border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded"
-                                        name="email"
-                                        value={email}
-                                        onChange={handleSetEmail}
-                                    ></input>
-                                    <input
-                                        id="password"
-                                        type="password"
-                                        placeholder="New Password"
-                                        className={`border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded`}
-                                        value={repass.pass}
-                                        onChange={e => validatePass(e.target.value, repass.rePass)}
+    return (
+        <>
+            <div
+                className="min-h-screen min-w-screen">
+                <div className="flex flex-col ">
+                    <div className="md:flex md:flex-row h-screen md:items-center justify-around">
+                        <div className="w-1/2 md:flex hidden flex-col items-center h-screen md:bg-[url('/assets/images/fill-form.png')] bg-center bg-no-repeat">
 
-                                    ></input>
-                                    <input
-                                        id="repassword"
-                                        type="password"
-                                        placeholder="Re-enter New Password"
-                                        className={`border px-3 py-2 text-gray-500 w-full shadow appearance-none ${repass.pass == repass.rePass || repass.rePass == '' ? '' : 'border-red-500'} focus:outline-none rounded`}
-                                        value={repass.rePass}
-                                        onChange={e => validatePass(repass.pass, e.target.value)}
-                                    ></input>
-                                    <button className="text-white block bg-sky-500 p-3 w-full font-medium disabled:bg-sky-700" onClick={signUp}
-                                        disabled={repass.pass == '' || repass.rePass == '' || email == '' || name == ''}
-                                    >Sign up</button>
-                                    <div className="flex flex-row justify-end w-full">
-                                        <Link href="login" className="tracking-wide" >Already have an account?</Link>
-                                    </div>
+                        </div>
+                        <div className="flex flex-col justify-center items-center bg-white shadow-lg rounded-md md:w-1/2 h-full">
+                            <div className="w-1/2 flex flex-col justify-center items-center gap-2">
+                                <strong className="text-3xl uppercase md:mb-4 block tracking-wide">Register</strong>
+                                <input
+                                    id="name"
+                                    type="text"
+                                    placeholder="Full Name"
+                                    className="border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded"
+                                    name="email"
+                                    value={name}
+                                    onChange={handleSetName}
+                                ></input>
+                                <input
+                                    id="email"
+                                    type="email"
+                                    placeholder="Enter Your Email"
+                                    className="border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded"
+                                    name="email"
+                                    value={email}
+                                    onChange={handleSetEmail}
+                                ></input>
+                                <input
+                                    id="password"
+                                    type="password"
+                                    placeholder="New Password"
+                                    className={`border px-3 py-2 text-gray-500 w-full shadow appearance-none focus:outline-none rounded`}
+                                    value={repass.pass}
+                                    onChange={e => validatePass(e.target.value, repass.rePass)}
+
+                                ></input>
+                                <input
+                                    id="repassword"
+                                    type="password"
+                                    placeholder="Re-enter New Password"
+                                    className={`border px-3 py-2 text-gray-500 w-full shadow appearance-none ${repass.pass == repass.rePass || repass.rePass == '' ? '' : 'border-red-500'} focus:outline-none rounded`}
+                                    value={repass.rePass}
+                                    onChange={e => validatePass(repass.pass, e.target.value)}
+                                ></input>
+                                <button className="text-white block bg-sky-500 p-3 w-full font-medium disabled:bg-sky-700" onClick={signUp}
+                                    disabled={repass.pass == '' || repass.rePass == '' || email == '' || name == ''}
+                                >Sign up</button>
+                                <div className="flex flex-row justify-end w-full">
+                                    <Link href="login" className="tracking-wide" >Already have an account?</Link>
                                 </div>
-
                             </div>
 
                         </div>
+
                     </div>
                 </div>
-            </>
-        )
-    } else {
-        router.push('/')
-    }
+            </div>
+        </>
+    )
+
 }
