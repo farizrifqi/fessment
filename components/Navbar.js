cc96ebdd42cad10d109d6b61eb2fd51bbc39f040
zeraneL <farizrifqi26@gmail.com>
2023-05-02 11:19:22 +0800

    add navbar

---
+++ components/Navbar.js
@@ -0,0 +1,48 @@
+import { signOut } from "next-auth/react"
+import { useRouter } from 'next/router';
+
+import Link from "next/link"
+
+export default function Navbar(props) {
+    const session = props.session
+    const router = useRouter()
+
+    const signOutClick = () => {
+        signOut()
+    }
+    const links = [
+        {
+            path: "/dashboard",
+            name: "Dashboard"
+        },
+        {
+            path: "/manage",
+            name: "Manage Accounts"
+        }
+    ]
+
+    if (session) {
+        return (
+            <div
+                className="fixed w-full bg-slate-800">
+                <div className="m-5 flex flex-row justify-between items-center text-gray-100">
+                    <div className="font-bold  tracking-wider text-3xl">
+                        <Link href='' className="text-white hover:opacity-100">FESSMENT.</Link>
+                    </div>
+
+                    <div className="flex flex-row gap-3">
+                        {
+                            links.map((l, i) => (
+                                <Link href={`${router.pathname == l.path ? "#" : l.path}`} key={i} className=" bg-slate-700 px-5 py-1 rounded hover:opacity-100 text-sm text-gray-200 font-medium">{l.name}</Link>
+                            ))
+                        }
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <div className="text-white px-5 py-2 opacity-100 text-sm border rounded border-slate-700"><b>{session.email}</b> </div>
+                        <button onClick={signOutClick} className="text-white bg-slate-700 px-5 py-2 rounded opacity-100 text-sm "><i className="fa fa-power-off"></i></button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,8 +3,7 @@ import { useSession, signIn, signOut, getSession } from "next-auth/react"
 import { useRouter } from 'next/router'
 import { useEffect } from "react"
 
-
-import Link from "next/link"
+import Navbar from "@/components/Navbar"
 
 export default function DashboardPage() {
     let { data: session, status } = useSession()
@@ -25,23 +24,7 @@ export default function DashboardPage() {
 
                 <div className="w-min-screen antialiased">
                     <div className="flex flex-col">
-                        <div
-                            className="fixed w-full bg-slate-800">
-                            <div className="m-5 flex flex-row justify-between items-center text-gray-100">
-                                <div className="font-bold  tracking-wider text-3xl">
-                                    <Link href='' className="text-white hover:opacity-100">FESSMENT.</Link>
-                                </div>
-
-                                <div className="flex flex-row gap-3">
-                                    <Link href="#" className=" bg-slate-700 px-5 py-1 rounded hover:opacity-100 text-sm text-gray-200 font-medium">Dashboard</Link>
-                                    <Link href="/manage" className=" bg-slate-700 px-5 py-1 rounded hover:opacity-100 text-sm text-gray-200 font-medium">Manage Accounts</Link>
-                                </div>
-                                <div className="flex flex-row gap-2">
-                                    <div className="text-white px-5 py-2 opacity-100 text-sm border rounded border-slate-700"><b>{session.email}</b> </div>
-                                    <button onClick={signOutClick} className="text-white bg-slate-700 px-5 py-2 rounded opacity-100 text-sm "><i className="fa fa-power-off"></i></button>
-                                </div>
-                            </div>
-                        </div>
+                        <Navbar session={session} />
                         <div className="h-screen flex flex-col justify-center items-center bg-slate-900">
 
                         </div>
