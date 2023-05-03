d8103d4021113af4819709057083b8559d99a422
zeraneL <farizrifqi26@gmail.com>
2023-05-04 00:24:44 +0800

    modify RunFessment and refreshToken response

--- pages/api/twitter/[...twitter].js
+++ pages/api/twitter/[...twitter].js
@@ -13,7 +13,6 @@ export default async function APITwitter(req, res) {
                 res.redirect(307, '/dashboard?type=warning&message=' + cb.message).end()
             }
             break;
-
         case "getUserTwitterAccounts":
             let accounts = await getUserTwitterAccounts(req)
             res.json(accounts)
@@ -32,8 +31,9 @@ export default async function APITwitter(req, res) {
             res.json(accs)
             break;
         case "RunFessment":
-            let runFess = await RunFessment()
-            res.json({ success: true })
+            if (!req.query.twitter[1]) { res.json({ success: false, message: "No Trigger" }) }
+            let runFess = await RunFessment(req.query.twitter[1])
+            res.json(runFess)
             break;
         default:
             res.json(req.query)
@@ -89,24 +89,35 @@ async function changeTriggers(req, id, key, time) {
 }
 async function refreshToken() {
     let accs = await HarperDBAdapter().getTwitterAccounts()
-    accs.forEach(async (acc) => {
+    let totAcc = 0, success = 0, fail = 0
+    for await (const acc of accs) {
+        totAcc++
         let getToken = await FessmentTwitter().refreshOAuthToken(acc.refresh_token)
-        let update = await HarperDBAdapter().updateAccounts(acc.id, { access_token: getToken.access_token, refresh_token: getToken.refresh_token })
-        return update
-    })
+        if (getToken.access_token) {
+            let update = await HarperDBAdapter().updateAccounts(acc.id, { access_token: getToken.access_token, refresh_token: getToken.refresh_token })
+            success++
+        } else {
+            fail++
+        }
+    }
+    return { totAcc: totAcc, success: success, fail: fail }
 }
-async function RunFessment() {
+async function RunFessment(triggerTime) {
     let accs = await HarperDBAdapter().getTwitterAccounts()
 
+    let success = 0, dupe = 0, fail = 0, image = 0, totAcc = 0
     accs.forEach(async (acc) => {
+        totAcc++
+        if (triggerTime != acc.triggerTime) return;
         let dms = await FessmentTwitter().getDirectMessages(acc.access_token)
-        dms.data.forEach(async (dm) => {
+        for await (const dm of dms.data) {
             if (dm.sender_id != acc.twitter_id) {
                 if (dm.text.includes(acc.triggerKey)) {
                     let log = await HarperDBAdapter().findRunLog(dm.id)
                     if (!(log && log[0])) {
                         let sendTweet, text
                         if (dm.attachments) {
+                            image++
                             return;
                             text = dm.text.split(" ")
                             text.pop()
@@ -117,18 +128,21 @@ async function RunFessment() {
                             text = dm.text
                             sendTweet = await FessmentTwitter().createTweet(acc.access_token, text)
                         }
-                        let status = "success"
                         console.log(sendTweet)
                         if (!sendTweet.data) {
-                            status = "fail"
+                            fail++
+                        } else {
+                            success++
                         }
                         let sendLog = await HarperDBAdapter().addRunLog(acc.id, dm.sender_id, dm.text, sendTweet.data.id, dm.id, "success")
-                        return true
+
                     } else {
+                        dupe++
                         console.log("pernah di post")
                     }
                 }
             }
-        })
+        }
     })
+    return { totAcc: totAcc, success: success, fail: fail, dupe: dupe, image: image }
 }
