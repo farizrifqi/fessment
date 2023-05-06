6c288163fff9e948df1ed05034a799f507b2402f
Fariz Rifqi <farizrifqi26@gmail.com>
2023-05-06 15:43:29 +0800

    grammar fix

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,10 @@ export default function Home() {
         <div className="flex flex-col">
           <div className="h-screen w-full flex flex-col justify-center items-center bg-slate-900">
             <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Confess your tweet</h1>
-            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:text-xs sm:px-16 xl:px-48 dark:text-gray-400 text-center">Here at Fessment we provide free menfess bot with simple management.</p>
+            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:text-xs sm:px-16 xl:px-48 dark:text-gray-400 text-center">
+              Here at Fessment, we provide free menfess bot with simple management.
+
+            </p>
 
             <div className="flex flex-col sm:flex-row w-3/4 sm:w-1/4 justify-around">
               <Link href="register" className="inline-flex items-center justify-center m-2 bg-sky-500 md:px-4 md:py-2 px-1 py-2 focus:shadow-outline tracking-wide text-xl md:text rounded hover:opacity-100 text-white w-full">Try it now</Link>
